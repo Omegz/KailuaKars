2a1584ee270199bf7b3b974969b8d50e4dfe5583
yasmine <yagh0001@stud.kea.dk>
2025-09-16 23:13:21 +0200

    start

--- Main.java
+++ Main.java
@@ -39,7 +39,7 @@ public class Main {
         try (Statement st = conn.createStatement();
              ResultSet rs = st.executeQuery(sql)) {
             while (rs.next()) {
-                System.out.printf("%d - %s (%s)%n",
+                System.out.printf("%d - - %s (%s)%n",
                         rs.getInt("renter_id"),
                         rs.getString("name"),
                         rs.getString("city"));
